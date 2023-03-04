b33b2af053258e4151b55570f1cd94bd2b0585f4
nodari-dev <nodari.pylypyshak@gmail.com>
2023-03-04 14:17:48 +0100

    root, styles

--- src/components/Root/Root.tsx
+++ src/components/Root/Root.tsx
@@ -4,17 +4,20 @@ import { Provider } from "react-redux";
 import { persistor, store } from "../../store";
 import { Loader as LoaderProvider } from "../Loader";
 import { Router } from "../Router";
+import styles from "./Root.module.scss"
 
 interface IProps {}
 
 export const Root: FC<IProps> = (props: IProps): JSX.Element => {
   return (
-    <Provider store={store}>
-      <PersistGate persistor={persistor} loading={null}>
-        <LoaderProvider>
-          <Router />
-        </LoaderProvider>
-      </PersistGate>
-    </Provider>
+      <div className={styles.root}>
+          <Provider store={store}>
+              <PersistGate persistor={persistor} loading={null}>
+                  <LoaderProvider>
+                      <Router />
+                  </LoaderProvider>
+              </PersistGate>
+          </Provider>
+      </div>
   );
 };
