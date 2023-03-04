b49fc764fb98ee3645a224785380036746e55722
nodari-dev <nodari.pylypyshak@gmail.com>
2023-03-04 14:53:47 +0100

    home page

--- src/layouts/Landing/Landing.tsx
+++ src/layouts/Landing/Landing.tsx
@@ -1,22 +1,14 @@
 import React, { FC } from "react";
-import { Footer, Header } from "../../components";
-import { classes } from "../../utils";
+import styles from "./Landing.module.scss"
 
 interface IProps {
-  main?: {
-    className?: string;
-  };
   children?: React.ReactNode | React.ReactNode[];
 }
 
-export const Landing: FC<IProps> = ({ main, children }: IProps): JSX.Element => {
+export const Landing: FC<IProps> = ({ children }: IProps): JSX.Element => {
   return (
-    <div>
-      <Header />
-      <main className={classes(main?.className)}>
+    <div className={styles.landing}>
         {children}
-      </main>
-      <Footer />
     </div>
   );
 };
