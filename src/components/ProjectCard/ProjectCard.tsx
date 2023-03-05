e002da986ff80b809ac0d12ee8e87f4319b31082
nodari-dev <nodari.pylypyshak@gmail.com>
2023-03-05 08:11:03 +0100

    connected view profile

--- src/components/ProjectCard/ProjectCard.tsx
+++ src/components/ProjectCard/ProjectCard.tsx
@@ -5,21 +5,18 @@ import { StarFilled, StarOutlined } from "@ant-design/icons";
 
 interface IProps {
     id: string;
-    rating: number;
     title: string;
     description: string;
 }
 
-export const ProjectCard = ({ id, rating, title, description, ...other }: IProps) => {
+export const ProjectCard = ({ id, title, description, ...other }: IProps) => {
     return (
         <Card
             {...other}
             key={id}
             extra={
                 <div>
-                    {shortenNumber(rating)}
-                    {0 < 1 ? <StarOutlined role='button' /> : <StarFilled role='button' />}
-                    <a href={id} style={{ marginLeft: 5 }}>
+                    <a href={`/project/${id}`} style={{ marginLeft: 5 }}>
                         More
                     </a>
                 </div>
--- src/pages/Profile/ProfileView.tsx
+++ src/pages/Profile/ProfileView.tsx
@@ -1,127 +1,65 @@
 import React, { FC, useState } from "react";
 import styles from "./Profile.module.scss";
-import { shortenNumber } from "../../utils/shortenNumber";
-import { StarFilled, StarOutlined } from "@ant-design/icons";
 import { ProjectCard } from "../../components/ProjectCard";
 import { UserAvatar } from "../../components/Avatar";
 
-interface IProps {}
+interface IProps {
+    id: string;
+    createdAt?: string;
+    gitHubAuthenticated: boolean;
+    email: string;
+    userName: string;
+    avatarImageLink: null | string;
+    cvLink: null | string;
+    firstName: string;
+    lastName: string;
+    experience: string;
+    projects: string[];
+    requests: string[];
+}
 
 export const ProfileView: FC<IProps> = (props: IProps): JSX.Element => {
-    const onFinish = (values: any) => {
-        console.log("Failed:", values);
-    };
-
-    const onFinishFailed = (errorInfo: any) => {
-        console.log("Failed:", errorInfo);
-    };
-
-    const profileData = {
-        image: "",
-        userName: "Daniel123123",
-        firstName: "Daniel",
-        lastName: "Penis",
-        email: "123@123.com",
-        github: "git",
-        linkedin: "link",
-        experience: "2",
-    };
-
-    const data = [
-        {
-            id: "qw233333e",
-            title: "JWP",
-            rating: 30123,
-            linkToLive: "",
-            description: "Description",
-        },
-        {
-            id: "qww2e",
-            title: "JWP",
-            rating: 30123,
-            linkToLive: "",
-            description: "Description",
-        },
-        {
-            id: "qwe2",
-            title: "JWP",
-            rating: 30123,
-            linkToLive: "",
-            description: "Description",
-        },
-        {
-            id: "qwe",
-            title: "JWP",
-            rating: 30123,
-            linkToLive: "",
-            description: "Description",
-        },
-        {
-            id: "qw313e",
-            title: "JWP",
-            rating: 30123,
-            linkToLive: "",
-            description: "Description",
-        },
-    ];
-
     return (
         <div className={styles.profileContent}>
             <div className={styles.profileContainerLarge}>
-                <h4>First name and last name</h4>
+                <h4>
+                    {props?.firstName} {props?.lastName}
+                </h4>
                 <div className={styles.profileInfo}>
-                    <UserAvatar
-                        className={styles.avatar}
-                        image={profileData.image}
-                        lastName={profileData.lastName}
-                        firstName={profileData.firstName}
-                    />
+                    <UserAvatar className={styles.avatar} image={props?.avatarImageLink} />
                     <div>
-                        <p>Username: {profileData.userName}</p>
-                        <p>Email: {profileData.email}</p>
-                        <p>Github: {profileData.github ? profileData.github : "-"}</p>
-                        <p>Linkedin: {profileData.linkedin ? profileData.linkedin : "-"}</p>
-                        <p>Experience: {profileData.experience}</p>
+                        <p>Username: {props?.userName}</p>
+                        <p>Email: {props?.email}</p>
+                        <p>Github: {props?.gitHubAuthenticated ? "YES" : "NO"}</p>
+                        <p>
+                            CV: <a href={props?.cvLink}>Open</a>
+                        </p>
+                        <p>Experience: {props?.experience}</p>
                     </div>
                 </div>
                 <div className={styles.profileSection}>
                     <h5>Last 3 projects:</h5>
                     <div className={styles.projectsGrid}>
-                        {data.slice(0, 3).map((item) => (
+                        {props?.projects?.slice(0, 3).map((item, index) => (
                             <ProjectCard
-                                key={item.id}
-                                id={item.id}
-                                title={item.title}
-                                description={item.description}
-                                rating={item.rating}
+                                key={item?.id}
+                                id={item?.id}
+                                title={item?.title}
+                                description={item?.description}
                             />
                         ))}
                     </div>
                 </div>
-                <div className={styles.profileSection}>
-                    <h5>Starred:</h5>
-                    <div className={styles.projectsGrid}>
-                        {data.map((item, index) => (
-                            <ProjectCard
-                                key={item.id}
-                                id={item.id}
-                                title={item.title}
-                                description={item.description}
-                                rating={item.rating}
-                            />
-                        ))}
-                    </div>
-                </div>
+
                 <div className={styles.profileSection}>
                     <h5>Collaborates in:</h5>
                     <div className={styles.projectsGrid}>
-                        {data.map((item, index) => (
+                        {props?.projects?.map((item, index) => (
                             <ProjectCard
-                                key={item.id}
-                                id={item.id}
-                                title={item.title}
-                                description={item.description}
-                                rating={item.rating}
+                                key={item?.id}
+                                id={item?.id}
+                                title={item?.title}
+                                description={item?.description}
                             />
                         ))}
                     </div>
