826c868768ae624b8de83dacfca204e5d6d7a5ce
nodari-dev <nodari.pylypyshak@gmail.com>
2023-03-05 03:13:16 +0100

    updated auth

--- src/pages/SignIn/SignIn.tsx
+++ src/pages/SignIn/SignIn.tsx
@@ -1,7 +1,7 @@
 import React, { FC } from "react";
 import { Authorization as AuthorizationLayout, Landing as LandingLayout } from "../../layouts";
 import { useApi, useAuthorization } from "../../hooks";
-import { Button, Form, Input } from "antd";
+import { Button, Form, Input, Result } from "antd";
 import { useNavigate } from "react-router-dom";
 import { Player } from "@lottiefiles/react-lottie-player";
 import animation from "../../components/Loader/Loader.animation.json";
@@ -9,64 +9,88 @@ import animation from "../../components/Loader/Loader.animation.json";
 interface IProps {}
 
 export const SignIn: FC<IProps> = (props: IProps): JSX.Element | null => {
-  const api = useApi();
-  const navigate = useNavigate();
-  const { isAuthorized, setAuthorization } = useAuthorization();
+    const api = useApi();
+    const navigate = useNavigate();
+    const { isAuthorized, setAuthorization } = useAuthorization();
 
-  const onFinish = (values: any) => {
-    api.authorization
-      .signUp({ ...values })
-      .then(({ refreshToken, jsonWebToken }) => {
-        api.account.info.get({ jsonWebToken, loader: "Getting user info..." })
-          .then((user) => setAuthorization(jsonWebToken, user, refreshToken));
-      })
-      .then(() => {
-        navigate("/profile");
-      });
-  };
+    const onFinish = (values: any) => {
+        api.authorization
+            .signUp({ ...values })
+            .then(({ refreshToken, jsonWebToken }) => {
+                api.account.info
+                    .get({ jsonWebToken, loader: "Getting user info..." })
+                    .then((user) => setAuthorization(jsonWebToken, user, refreshToken));
+            })
+            .then(() => {
+                navigate("/profile");
+            });
+    };
 
-  const onFinishFailed = (errorInfo: any) => {
-    console.log("Failed:", errorInfo);
-  };
+    const onFinishFailed = (errorInfo: any) => {
+        console.log("Failed:", errorInfo);
+    };
 
-  return !isAuthorized ? (
-    <AuthorizationLayout>
-      <Player src={animation} style={{ width: 100 }} autoplay loop />
-      <h5>Sign in</h5>
-      <Form
-        name="basic"
-        initialValues={{ username: "", password: "" }}
-        onFinish={onFinish}
-        onFinishFailed={onFinishFailed}
-        autoComplete="off"
-      >
-        <Form.Item
-          name="username"
-          rules={[ { required: true, message: "Please input your username!" } ]}
-        >
-          <Input placeholder="Username" />
-        </Form.Item>
+    return !isAuthorized ? (
+        <AuthorizationLayout>
+            <Player src={animation} style={{ width: 100 }} autoplay loop />
+            <h5>Sign in</h5>
+            <Form
+                name='basic'
+                initialValues={{ username: "", password: "" }}
+                onFinish={onFinish}
+                onFinishFailed={onFinishFailed}
+                autoComplete='off'
+            >
+                <Form.Item
+                    name='username'
+                    rules={[{ required: true, message: "Please input your username!" }]}
+                >
+                    <Input placeholder='Username' />
+                </Form.Item>
 
-        <Form.Item
-          name="password"
-          rules={[ { required: true, message: "Please input your password!" } ]}
-        >
-          <Input.Password placeholder={"Password"} />
-        </Form.Item>
+                <Form.Item
+                    name='password'
+                    rules={[{ required: true, message: "Please input your password!" }]}
+                >
+                    <Input.Password placeholder={"Password"} />
+                </Form.Item>
 
-        <Form.Item>
-          <Button type="primary" htmlType="submit">
-            Log in
-          </Button>
-        </Form.Item>
-        <p>
-          Have no account? <a onClick={() => navigate("/sign-up")}>Sign up!</a>
-        </p>
-      </Form>
-    </AuthorizationLayout>
-  ) : (
-    <LandingLayout>
-      <div>You are authorized!</div>
-    </LandingLayout>
-  );
+                <Form.Item
+                    style={{
+                        alignItems: "center",
+                        display: "flex",
+                        flexDirection: "column",
+                        textAlign: "center",
+                        width: "100%",
+                    }}
+                >
+                    <Button type='primary' htmlType='submit'>
+                        Log in
+                    </Button>
+                    <p>or</p>
+                    <Button
+                        href='https://backend-flu4w4sfwa-lz.a.run.app/api/users/registration/github'
+                        target='_blank'
+                    >
+                        Continue with GitHub
+                    </Button>
+                </Form.Item>
+                <p>
+                    Have no account? <a onClick={() => navigate("/sign-up")}>Sign up!</a>
+                </p>
+            </Form>
+        </AuthorizationLayout>
+    ) : (
+        <LandingLayout>
+            <Result
+                status='success'
+                title='You are authorized!'
+                extra={[
+                    <Button type='primary' onClick={() => navigate("/for-you")}>
+                        Proceed
+                    </Button>,
+                ]}
+            />
+        </LandingLayout>
+    );
 };
